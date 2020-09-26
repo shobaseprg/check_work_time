9d7d12b05770ac7e345f6a2dac839b5096c7bb99
shobaseprg <pgymlsho19128@gmail.com>
2020-09-26 23:18:53 +0900

    display detail

--- calendar.php
+++ calendar.php
@@ -24,31 +24,37 @@ $defineWorkDay = 0;
 
   <body>
     <h1><?php echo $_SESSION['userName']."さん"; ?></h1>
-      <a href= 'edit.php'>カレンダー編集</a>
-      <a href= 'join/logout.php'>ログアウト</a>
-      <br>
+    <a href= 'edit.php'>カレンダー編集</a>
+    <a href= 'join/logout.php'>ログアウト</a>
+    <br>
+    <br>
 <!-- ===================================
 入力フォーム
 =================================== -->
-      <div class="inputTime">
-        <form action = "" method='POST'>
-        <?php  echo date("Y年m月d日"); ?>終了時点の労働時間入力
-          <input type='number' name='lackTimeHour' min=0 max= 999 value=0>時間 <!-- 不足時間 -->
-          <input type='number' name='lackTimeMinit' min=0 max=60 value=0>分 <!-- 不足分 -->
-          <input type='submit' name="calculate" value="計算する">
-        </form>
-      </div>
+    <div class="inputTime">
+      <form action = "" method='POST'>
+      <?php  echo date("Y年m月d日"); ?>終了時点の労働時間入力
+        <input type='number' name='lackTimeHour' min=0 max= 999 value=0>時間 <!-- 不足時間 -->
+        <input type='number' name='lackTimeMinit' min=0 max=60 value=0>分 <!-- 不足分 -->
+        <input type='submit' name="calculate" value="計算する">
+      </form>
+    </div>
+
+<!-- ===================================
+// 計算結果
+// =================================== -->
     <?php
       if (!empty($_POST['calculate'])){
         $today = (int)date("d");
         $fromTodayWorkTime = 0;
+        $fromTodayWorkDay = 0;
         for($i=1; $i < $today + 1; $i++) {
           if ($saveCalendar[$i."d"] == 0 ) { // 平日だった場合
             $fromTodayWorkTime += 8;
+            $fromTodayWorkDay += 1;
             }
           }
         $inuptTimeMinit = changeMimit($_POST['lackTimeHour'], $_POST['lackTimeMinit']);
-
         $result = ((int)$saveCalendar['lackTime'] + ($fromTodayWorkTime * 60)) - $inuptTimeMinit;
         if ($result > 0) {
           $is_over = "不足";
@@ -56,8 +62,14 @@ $defineWorkDay = 0;
           $is_over = "超過";
         }
         $resultAbs = abs($result);
-
-        echo "<br><div class='result'>".changeHour($resultAbs).$is_over."</div>";
+        echo "<br><div class='result'>".changeHour($resultAbs).$is_over."</div><br>";
+        echo "<div class='detail'>";
+        echo "本日までの労働日数：".$fromTodayWorkDay."日<br> ";
+        echo "本日までの所定労働時間：".$fromTodayWorkTime."時間00分<br>";
+        echo "先月までの不足時間：".changeHour((int)$saveCalendar['lackTime'])."<br>";
+        echo "本日までの必要労働時間：".changeHour(((int)$saveCalendar['lackTime'] + ($fromTodayWorkTime * 60)))."<br>";
+        echo "本日までの労働時間".changeHour(inuptTimeMinit);
+        echo "</div>";
 
         }
         for($i=1; $i < $saveCalendar['lastday'] + 1; $i++) {
@@ -75,12 +87,6 @@ $defineWorkDay = 0;
             $defineWorkDay += 1;
           }
         }
-        
-          fromDayToMinit($defineWorkDay);
-          echo "所定労働日数  ".$defineWorkDay;
-          echo "所定労働時間  ".fromDayToMinit($defineWorkDay);
-          echo "先月不足時間  ".$saveCalendar['lackTime'];
-          echo "必要労働時間".(fromDayToMinit($defineWorkDay) + $saveCalendar['lackTime']);
         ?>
   </body>
 </html>
